f4158c299b7606519ff2980c565d48cdeb07029a
student <swanjikumaina@gmail.com>
2023-03-07 17:33:33 +0300

    corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+//1.Create a class called Human with these attributes: name, age, weight. It has the following functions:
+//eat(foodWeight: Int) :Prints “I am eating {foodWeight} kgs of food” and increments the human’s weight by the weight of the food eaten (3 points)
+//speak(speech: String) :Prints the string passed to it (2 points)
+//birthday( ) :Increments the human’s age by 1 (2 points)
+//Create an instance of this human class and invoke all its functions
+//2. Create a data class User with these fields: firstName, lastName, email, phoneNumber, password. Create an instance of User and print out any 2 attributes (3 points)
+
 
 fun main(){
     val mother = Human("Julie",36,75)
@@ -16,12 +23,13 @@ fun main(){
 }
 
 
-
+//weight should be a double because of the mordern scale.
 class Human(var name:String,var age:Int,var weight:Int){
     fun eat(foodWeight:Int){
         println("I am eating $foodWeight kgs of food")
         weight = weight + foodWeight
-    }
+//        also weight +=foodweight
+   }
     fun speak(speech:String){
     name  = speech
     }
